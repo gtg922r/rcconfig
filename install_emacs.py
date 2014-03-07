8f39d16385c9956690013ac3ef05ac12d386b216
Ryan Cash <rc@ryancash.com>
2014-03-06 16:35:29 -0800

    Download and install cask automatically

--- install_emacs.py
+++ install_emacs.py
@@ -3,6 +3,7 @@
 
 import sys
 import os
+import string
 
 home = os.path.expanduser('~')
 
@@ -11,7 +12,12 @@ if os.path.exists(os.path.join(home, '.emacs/init.el')):
 elif os.path.exists(os.path.join(home, '.emacs.d')):
     print '.emacs.d already exists'
 else:
+    print 'Linking .emacs.d'
     os.symlink(os.path.join(os.getcwd(), '.emacs.d'), os.path.join(home, '.emacs.d'))
+    print 'Installing cask...'
     os.system('curl -fsSkL https://raw.github.com/cask/cask/master/go | python')
-    print 'Now export the path as instructed, then enter .emacs.d and `cask install`'
+    os.chdir(os.path.join(os.getcwd(), '.emacs.d'))
+    print 'Installing packages from cask file...'
+    os.system(os.path.join(home, '.cask/bin/cask') + ' install')
+    print 'Emacs ready for use!'
 
