58a9d94304bcf43e7e49b85884b9b976eecd7f32
Ryan Cash <ryan@wrightspeed.com>
2014-04-07 18:56:06 -0700

    Allow optional argument to specify directory

--- scripts/pandoc_run.sh
+++ scripts/pandoc_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find . -name "*md" | while read FILE
+find ${1:-.} -name "*md" | while read FILE
 do
     echo Converting \"$FILE\" from md to html5...
     pandoc -f markdown_github -t html5 --mathjax -H ~/.emacs.d/markdown/style_include.css  -o "${FILE%.*}".html "$FILE"
